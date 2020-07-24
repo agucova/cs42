581a3e60d50f11ce6a58a588fbb8b31a1c16d6df
Agustín Covarrubias <agucova@uc.cl>
2020-07-23 23:50:36 -0400

    Añadir numeros pythonescos

---
+++ numeros-pythonescos/__init__.py
@@ -0,0 +1,146 @@
+import check50
+
+@check50.check()
+def numero_0():
+    """numero_0"""
+    check50.run("python3 numeros_pythonescos.py").stdin("5", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_1():
+    """numero_1"""
+    check50.run("python3 numeros_pythonescos.py").stdin("1", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_2():
+    """numero_2"""
+    check50.run("python3 numeros_pythonescos.py").stdin("7", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_3():
+    """numero_3"""
+    check50.run("python3 numeros_pythonescos.py").stdin("130", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_4():
+    """numero_4"""
+    check50.run("python3 numeros_pythonescos.py").stdin("131", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_5():
+    """numero_5"""
+    check50.run("python3 numeros_pythonescos.py").stdin("167", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_6():
+    """numero_6"""
+    check50.run("python3 numeros_pythonescos.py").stdin("193", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_7():
+    """numero_7"""
+    check50.run("python3 numeros_pythonescos.py").stdin("191", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_8():
+    """numero_8"""
+    check50.run("python3 numeros_pythonescos.py").stdin("105", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_9():
+    """numero_9"""
+    check50.run("python3 numeros_pythonescos.py").stdin("1920", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_10():
+    """numero_10"""
+    check50.run("python3 numeros_pythonescos.py").stdin("1967", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_11():
+    """numero_11"""
+    check50.run("python3 numeros_pythonescos.py").stdin("1932", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_12():
+    """numero_12"""
+    check50.run("python3 numeros_pythonescos.py").stdin("153117", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_13():
+    """numero_13"""
+    check50.run("python3 numeros_pythonescos.py").stdin("150448", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_14():
+    """numero_14"""
+    check50.run("python3 numeros_pythonescos.py").stdin("123487788", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_15():
+    """numero_15"""
+    check50.run("python3 numeros_pythonescos.py").stdin("123487789", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_16():
+    """numero_16"""
+    check50.run("python3 numeros_pythonescos.py").stdin("123487887", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_17():
+    """numero_17"""
+    check50.run("python3 numeros_pythonescos.py").stdin("123487878", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_18():
+    """numero_18"""
+    check50.run("python3 numeros_pythonescos.py").stdin("16", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_19():
+    """numero_19"""
+    check50.run("python3 numeros_pythonescos.py").stdin("13", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_20():
+    """numero_20"""
+    check50.run("python3 numeros_pythonescos.py").stdin("2210", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_21():
+    """numero_21"""
+    check50.run("python3 numeros_pythonescos.py").stdin("2206", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_22():
+    """numero_22"""
+    check50.run("python3 numeros_pythonescos.py").stdin("172738", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_23():
+    """numero_23"""
+    check50.run("python3 numeros_pythonescos.py").stdin("172751", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_24():
+    """numero_24"""
+    check50.run("python3 numeros_pythonescos.py").stdin("172794", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_25():
+    """numero_25"""
+    check50.run("python3 numeros_pythonescos.py").stdin("172716", prompt=False).stdout("False", regex=False).exit(0)
+
+@check50.check()
+def numero_26():
+    """numero_26"""
+    check50.run("python3 numeros_pythonescos.py").stdin("1000", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_27():
+    """numero_27"""
+    check50.run("python3 numeros_pythonescos.py").stdin("100000", prompt=False).stdout("True", regex=False).exit(0)
+
+@check50.check()
+def numero_28():
+    """numero_28"""
+    check50.run("python3 numeros_pythonescos.py").stdin("100027998", prompt=False).stdout("True", regex=False).exit()
